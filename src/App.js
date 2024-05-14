a86629a90aedb4cecb90bb643d95d9396b0c39aa
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-05-14 15:00:55 +0530

    Netflix final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './Components/Navbar/NavBar.js'
 import './App.css'
+import {originals,action,Romance,Comedy,Horror} from './urls.js'
 import Banner from './Components/Banner/Banner.js'
 import RowPost from './Components/RowPost/RowPost.js'
 
@@ -9,8 +10,11 @@ function App() {
     <div>
       <NavBar/>
       <Banner/>
-      <RowPost/>
-
+      <RowPost url = {originals} title = 'Netflix Originals'/>
+      <RowPost url = {action} title = 'Action' isSmall/>
+      <RowPost url = {Romance} title = 'Romance' isSmall/>
+      <RowPost url = {Comedy} title = 'Comedy' isSmall/>
+      <RowPost url = {Horror} title = 'Horror' isSmall/>
     </div>
   )
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "3b1c5f27dd2d160ca91630721b07cb43";
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
